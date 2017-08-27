8be50f6944fb8f55b2deec50a54ff767890f0015
Deuana <deuana.telles@hotmail.com>
2017-08-26 22:25:05 -0300

    Edição do usuário

--- app/src/main/java/br/com/reclameufba/LoginActivity.java
+++ app/src/main/java/br/com/reclameufba/LoginActivity.java
@@ -1,11 +1,13 @@
 package br.com.reclameufba;
 
 import android.app.Activity;
-import android.app.AlertDialog;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import Controller.UsuarioController;
 import modelo.Usuario;
@@ -14,53 +16,47 @@ public class LoginActivity extends Activity {
     private EditText campoUsuario, campoSenha;
     private Context context;
     private UsuarioController usuarioController;
-    private AlertDialog.Builder alert;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         context = this;
         usuarioController = UsuarioController.getInstance(context);
         campoUsuario = (EditText) findViewById(R.id.login_usuario);
         campoSenha = (EditText) findViewById(R.id.login_senha);
+
+        Usuario usuario = new Usuario(1, "adm", "adm");
         try {
-            testaInicializacao();
+            usuarioController.insert(usuario);
         } catch (Exception e) {
-            exibeDialogo("Erro inicializando banco de dados");
             e.printStackTrace();
         }
+
+        Button entrarBtn = (Button) findViewById(R.id.login_entrar);
+        entrarBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentListaPosts = new Intent(LoginActivity.this, ListaPostsActivity.class);
+                startActivity(intentListaPosts);
+            }
+        });
     }
 
-    public void testaInicializacao() throws Exception {
-        if (usuarioController.findAll().isEmpty()) {
-            Usuario usuario = new Usuario(null, "adm", "adm");
-            usuarioController.insert(usuario);
-        }
-
-    }
-
-
-    public void exibeDialogo(String mensagem) {
-        alert = new AlertDialog.Builder(context);
-        alert.setPositiveButton("OK", null);
-        alert.setMessage(mensagem);
-        alert.create().show();
-    }
     public void validar(View view) {
         String usuario = campoUsuario.getText().toString();
         String senha = campoSenha.getText().toString();
         try {
             boolean isValid = usuarioController.validacao(usuario, senha);
             if (isValid) {
-                exibeDialogo("Usuario e senha validados com sucesso!");
+                Toast.makeText(LoginActivity.this, "Usuario e senha validados com sucesso!", Toast.LENGTH_SHORT).show();
             } else {
-                exibeDialogo("Verifique usuario e senha!");
+                Toast.makeText(LoginActivity.this,"Verifique usuario e senha!", Toast.LENGTH_SHORT).show();
             }
         } catch (Exception e) {
-            exibeDialogo("Erro validando usuario e senha");
+            Toast.makeText(LoginActivity.this,"Erro validando usuario e senha", Toast.LENGTH_SHORT).show();
             e.printStackTrace();
         }
     }
-
 }
